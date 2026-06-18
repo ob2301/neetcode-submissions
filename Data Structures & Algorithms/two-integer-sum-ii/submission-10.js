e9fed703a87d2a9eadde2910343ebc0b314ecaba
Omar <omarcopyb@gmail.com>
2026-06-18 10:13:01 -0700

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let L = 0, R = numbers.length - 1;
+
+        while(L < R){
+            const [left, right] = [numbers[L], numbers[R]];
+
+            if(left + right === target){
+                return [L + 1, R + 1];
+            }
+
+            if(left + right > target){
+                R--;
+            }
+
+            else{
+                L++;
+            }
+        }
+
+        return [];
+    }
+}
