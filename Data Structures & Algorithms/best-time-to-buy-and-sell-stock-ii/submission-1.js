d802a5432f413d8709760ba6bb647c6da7949af9
Omar <omarcopyb@gmail.com>
2026-06-18 10:31:27 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let profit = 0;
+
+        for(let i = 1; i < prices.length; i++){
+            if(prices[i - 1] < prices[i]){
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+
+        return profit;
+    }
+}
