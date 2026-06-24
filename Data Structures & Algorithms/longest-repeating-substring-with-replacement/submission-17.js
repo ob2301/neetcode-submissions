13dd1165b14a4db2a837703fe3593094c34b9020
Omar <omarcopyb@gmail.com>
2026-06-24 10:36:54 -0500

    Add: longest-repeating-substring-with-replacement - submission-17

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-17.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        
+        let freq = new Map(), max = 0;
+        if(s.length == 0) return 0;
+
+        let L = 0, R = 0;
+        let longest = 1;
+
+        while(R < s.length){
+            freq.set(s[R], (freq.get(s[R]) || 0) + 1);
+
+            max = Math.max(max, Math.max(...freq.values()));
+
+            while(R - L + 1 > max + k){
+                if(freq.get(s[L]) > 1) freq.set(s[L], freq.get(s[L]) - 1);
+                else{
+                    freq.delete(s[L]);
+                }
+                L++;
+            }
+            longest = Math.max(longest, R - L + 1);
+
+            R++;
+        }
+
+        return longest;
+    }
+}
