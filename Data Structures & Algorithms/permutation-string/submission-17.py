358ae582e44807720ddc0125c3bef2f166bc657f
Omar <omarcopyb@gmail.com>
2026-08-04 14:48:46 -0700

    Add: permutation-string - submission-17

---
+++ Data Structures & Algorithms/permutation-string/submission-17.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        m = {}
+
+        for letter in s1:
+            if letter in m:
+                m[letter] += 1
+            else:
+                m[letter] = 1
+        
+        i = 0
+        while i < len(s2):
+            left = i
+            freq = m.copy()
+
+            while left < len(s2) and left < len(s1) + i:
+                if s2[left] in freq:
+                    if freq[s2[left]] > 1:
+                        freq[s2[left]] -= 1
+                    else:
+                        del freq[s2[left]]
+                left += 1
+            
+            if not freq:
+                return True
+            
+            i += 1
+        return False
+                
+
