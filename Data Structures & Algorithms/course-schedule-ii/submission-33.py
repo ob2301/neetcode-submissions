0e55e90d8565b8165d4f231e521067df913ad5b9
Omar <omarcopyb@gmail.com>
2026-08-27 13:07:07 -0700

    Add: course-schedule-ii - submission-33

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-33.py
@@ -0,0 +1,47 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {}
+
+        for i in range(numCourses):
+            adj[i] = []
+
+        for a, b in prerequisites:
+            adj[a].append(b)
+
+        res = []
+        seen = set()
+
+        def dfs(course, localSeen):
+            nonlocal seen
+
+            if course in seen:
+                return True
+            
+            if course in localSeen:
+                return False #cycle detected
+            
+            localSeen.add(course)
+
+            for pre in adj[course]:
+                if not dfs(pre, localSeen):
+                    return False
+            
+            res.append(course)
+            seen.add(course)
+            return True
+
+        for i in range(numCourses):
+            if not i in seen:
+                dfs(i, set())
+
+        if not len(res) == numCourses:
+            return []
+
+        return res
+
+        
+
+            
+
+
+        
